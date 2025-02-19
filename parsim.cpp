2b53aa1e0277d26148f2d2965f5d38cc3936b1cc
Gonçalo Rua <goncalonrua@tecnico.ulisboa.pt>
2025-02-19 12:14:14 +0000

    add main function

--- parsim.cpp
+++ parsim.cpp
@@ -1,5 +1,8 @@
 #define _USE_MATH_DEFINES
+
 #include <math.h>
+#include <omp.h>
+
 #define G 6.67408e-11
 #define EPSILON2 (0.005 * 0.005)
 #define DELTAT 0.1
@@ -49,4 +52,15 @@ void init_particles(long seed, double side, long ncside, long long n_part,
 
     par[i].m = rnd01() * 0.01 * (ncside * ncside) / n_part / G * EPSILON2;
   }
+}
+
+int main(int argc, char *argv[]) {
+  double exec_time;
+  init_particles(...);
+  exec_time = -omp_get_wtime();
+  simulation();
+  exec_time += omp_get_wtime();
+  fprintf(stderr, "%.1fs\n", exec_time);
+  print_result();
+  // to the stdout!
 }
